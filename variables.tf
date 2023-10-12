1c52fdcfee88a3a2cb35442fea28ed643457d172
Kaung Myat Han <arkarhtethan@gmail.com>
2023-10-12 16:33:26 +0630

    update(): modularization finished

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable private_key_location {}
+variable public_key_location {}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable image_name {}
